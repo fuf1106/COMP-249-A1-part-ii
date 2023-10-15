19b318efd2168b484c132ee5d80bc2bea5cbb50a
Diana <115322193+VinVoide@users.noreply.github.com>
2023-10-14 23:35:00 -0400

    Commented Tram.java

--- COMP-249-A1-part-ii/src/Train_And_Tram/Tram.java
+++ COMP-249-A1-part-ii/src/Train_And_Tram/Tram.java
@@ -7,18 +7,33 @@
 package Train_And_Tram;
 import Metro.Metro;
 public class Tram extends Metro {
-
+    //Instance variables
     private int yearOfCreation;
     private static long serialNumberGenerator = 30000;
-    //Constructors
+    /**
+     * Default constructor constructs a new default Tram object
+     */    
     public Tram() {
         this.yearOfCreation = 0;
     }
+    /**
+     * Parameterized constructor constructs a new Tram object
+     * @param numberOfWheels number of wheels
+     * @param maxSpeed maximum speed
+     * @param numberOfVehicles number of vehicles
+     * @param startingStationName starting station name
+     * @param destinationStationName destination station name
+     * @param totalStops total number of stops
+     * @param yearOfCreation year of creation
+     */
     public Tram(int numberOfWheels, double maxSpeed, int numberOfVehicles, String startingStationName, String destinationStationName, int totalStops, int yearOfCreation) {
         super(numberOfWheels, maxSpeed, numberOfVehicles, startingStationName, destinationStationName, totalStops);
         this.yearOfCreation = yearOfCreation;
     }
-
+    /**
+     * Copy constructor constructs a copy of a Tram object
+     * @param oldTram takes a Metro object
+     */
     public Tram(Tram oldTram) {
         super(oldTram);
         this.yearOfCreation = oldTram.yearOfCreation;
@@ -28,19 +43,31 @@ public class Tram extends Metro {
     public int getYearOfCreation() {
         return yearOfCreation;
     }
-
     public void setYearOfCreation(int yearOfCreation) {
         this.yearOfCreation = yearOfCreation;
     }
-
+      /**
+     * @return the next serial number
+     */
+    @Override
     public long getNextSerialNumber(){
-
         return Tram.serialNumberGenerator++;
     }
+    /**
+     * @return a String outputting the information of the current object
+     */
     @Override
     public String toString(){
         return "This tram" + super.toString().substring(10, super.toString().length() -1) + " and was created in the year " + this.yearOfCreation + ".";
     }
+    /**
+     * This method checks if the object the method is called on is equal to the Object parameter
+     * It first checks if the provided object is null, and if not, compares the equality of the two objects
+     * If they are not equal, the method returns false
+     * If they are equal, then the object is cast as a Tram and returns true
+     * @param obj Takes an Object
+     * @return true or false
+     */
     public boolean equals(Object obj){
         if (obj == null) {
             return false;
@@ -53,7 +80,9 @@ public class Tram extends Metro {
             return (this.yearOfCreation == otherTram.yearOfCreation);
         }
     }
-
+    /**
+ 	* This method creates a copy of the current object
+	*/
     public Tram copy() {
         return new Tram(this);
     }
