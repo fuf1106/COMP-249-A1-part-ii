99319970efc38eee7746e6f9f09ac397f4f66fbf
Diana <115322193+VinVoide@users.noreply.github.com>
2023-10-14 23:28:46 -0400

    Commented Metro.java

--- COMP-249-A1-part-ii/src/Metro/Metro.java
+++ COMP-249-A1-part-ii/src/Metro/Metro.java
@@ -8,18 +8,32 @@ package Metro;
 import Train_And_Tram.Train;
 
 public class Metro extends Train {
-
+    //Instance variables
     private int totalStops;
     private static long serialNumberGenerator = 25000;
-    //Constructors
+    /**
+     * Default constructor constructs a new default Metro object
+     */
     public Metro() {
         this.totalStops = 0;
     }
+    /**
+     * Parameterized constructor constructs a new Metro object
+     * @param numberOfWheels number of wheels
+     * @param maxSpeed maximum speed
+     * @param numberOfVehicles number of vehicles
+     * @param startingStationName starting station name
+     * @param destinationStationName destination station name
+     * @param totalStops total number of stops
+     */
     public Metro(int numberOfWheels, double maxSpeed, int numberOfVehicles, String startingStationName, String destinationStationName, int totalStops) {
         super(numberOfWheels, maxSpeed, numberOfVehicles, startingStationName, destinationStationName);
         this.totalStops = totalStops;
     }
-
+    /**
+     * Copy constructor constructs a copy of a Metro object
+     * @param oldMetro takes a Metro object
+     */
     public Metro(Metro oldMetro) {
         super(oldMetro);
         this.totalStops = oldMetro.totalStops;
@@ -29,19 +43,32 @@ public class Metro extends Train {
     public int getTotalStops() {
         return totalStops;
     }
-
     public void setTotalStops(int totalStops) {
         this.totalStops = totalStops;
     }
+    /**
+     * @return the next serial number
+     */
     @Override
     public long getNextSerialNumber(){
         return Metro.serialNumberGenerator++;
     }
-
+    /**
+     * @return a String outputting the information of the current object
+     */
     @Override
     public String toString(){
         return "This metro" + super.toString().substring(10) + "It has " + this.totalStops + " total stops.";
     }
+    /**
+     * This method checks if the object the method is called on is equal to the Object parameter
+     * It first checks if the provided object is null, and if not, compares the equality of the two objects
+     * If they are not equal, the method returns false
+     * If they are equal, then the object is cast as a Metro and returns true
+     * @param obj Takes an Object
+     * @return true or false
+     */
+    @Override
     public boolean equals(Object obj){
         if (obj == null) {
             return false;
@@ -54,7 +81,9 @@ public class Metro extends Train {
             return (this.totalStops == otherMetro.totalStops);
         }
     }
-
+    /**
+ 	* This method creates a copy of the current object
+	*/
     public Metro copy() {
         return new Metro(this);
     }
