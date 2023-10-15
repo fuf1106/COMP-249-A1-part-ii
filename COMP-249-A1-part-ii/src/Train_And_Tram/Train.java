27d7619062ccab96b10affea86cf09c29a1407fd
Diana <115322193+VinVoide@users.noreply.github.com>
2023-10-14 23:33:00 -0400

    Commented Train.java

--- COMP-249-A1-part-ii/src/Train_And_Tram/Train.java
+++ COMP-249-A1-part-ii/src/Train_And_Tram/Train.java
@@ -8,25 +8,39 @@ package Train_And_Tram;
 import WheeledTransportation.WheeledTransportation;
 
 public class Train extends WheeledTransportation {
+    //Instance variables
     private int numberOfVehicles;
     private String startingStationName;
     private String destinationStationName;
 
     private static long serialNumberGenerator = 10000;
 
-    //Constructors
+    /**
+     * Default constructor constructs a new default Train object
+     */
     public Train() {
         this.numberOfVehicles = 0;
         this.startingStationName = "Starting Destination";
         this.destinationStationName = "Finish Destination";
     }
+    /**
+     * Parameterized constructor constructs a new Train object
+     * @param numberOfWheels number of wheels
+     * @param maxSpeed maximum speed
+     * @param numberOfVehicles number of vehicles
+     * @param startingStationName starting station name
+     * @param destinationStationName destination station name
+     */
     public Train(int numberOfWheels, double maxSpeed, int numberOfVehicles, String startingStationName, String destinationStationName) {
         super(numberOfWheels, maxSpeed);
         this.numberOfVehicles = numberOfVehicles;
         this.startingStationName = startingStationName;
         this.destinationStationName = destinationStationName;
     }
-
+    /**
+     * Copy constructor constructs a copy of a Train object
+     * @param oldTrain takes a Metro object
+     */
     public Train(Train oldTrain) {
         super(oldTrain);
         this.numberOfVehicles = oldTrain.numberOfVehicles;
@@ -38,33 +52,37 @@ public class Train extends WheeledTransportation {
     public int getNumberOfVehicles() {
         return numberOfVehicles;
     }
-
     public void setNumberOfVehicles(int numberOfVehicles) {
         this.numberOfVehicles = numberOfVehicles;
     }
-
     public String getStartingStationName() {
         return startingStationName;
     }
-
     public void setStartingStationName(String startingStationName) {
         this.startingStationName = startingStationName;
     }
-
     public String getDestinationStationName() {
         return destinationStationName;
     }
-
     public void setDestinationStationName(String destinationStationName) {
         this.destinationStationName = destinationStationName;
     }
-
+    /**
+     * @return the next serial number
+     */
     @Override
     public long getNextSerialNumber(){
 
         return Train.serialNumberGenerator++;
     }
-
+     /**
+     * This method checks if the object the method is called on is equal to the Object parameter
+     * It first checks if the provided object is null, and if not, compares the equality of the two objects
+     * If they are not equal, the method returns false
+     * If they are equal, then the object is cast as a Train and returns true
+     * @param obj Takes an Object
+     * @return true or false
+     */
     public boolean equals(Object obj){
         if (obj == null) {
             return false;
@@ -78,12 +96,17 @@ public class Train extends WheeledTransportation {
                     this.startingStationName.equals(otherTrain.startingStationName)  && this.destinationStationName.equals(otherTrain.destinationStationName));
         }
     }
+    /**
+     * @return a String outputting the information of the current object
+     */
     @Override
     public String toString(){
         return "This train" + super.toString().substring(27) + ". It has " + this.numberOfVehicles + " vehicles, starts at station " +
                 this.startingStationName + " and ends at station " + this.destinationStationName + ". ";
     }
-
+    /**
+ 	* This method creates a copy of the current object
+	*/
     public Train copy() {
         return new Train(this);
     }
